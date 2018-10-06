4b1ffdb392c37fab79d537e3d742e39cacaab04b
Taylor Newill <17301351+tanewill@users.noreply.github.com>
2018-10-06 19:27:00 +0900

    Update get_files.sh

--- get_files.sh
+++ get_files.sh
@@ -21,7 +21,7 @@ mkdir ~/hpcg
 cd ~/hpcg
 wget https://objectstorage.us-phoenix-1.oraclecloud.com/p/zptF4eHuxCyS5UsBjiiIIegTzySAXdf4BOdy_HGGWYQ/n/hpc/b/HPC_BENCHMARKS/o/hpcg-bin.tgz -O - | tar zx
 cd hpcg-bin
-mpirun -np 3 -ppn 3 -hostfile $hostlist ./xhpcg
+#mpirun -np 3 -ppn 3 -hostfile $hostlist ./xhpcg
 #GFLOPS
 grep VALID HPCG-Benchmark* | awk '{ print $10 }'
 
@@ -30,16 +30,16 @@ mkdir ~/hpl
 cd ~/hpl
 wget https://objectstorage.us-phoenix-1.oraclecloud.com/p/QL7Bbs7G1OqPM5i4nHTxn7FzCZh2xuPC2H3eebzGCMg/n/hpc/b/HPC_BENCHMARKS/o/hpl.tgz -O - | tar zx
 cd ~/hpl/hpl
-mpirun -np 2 -perhost 2 -hostfile $hostlist ./xhpl_intel64_static -n $linpack_N -p $linpack_P -q $linpack_Q -nb $linpack_NB > hpl_output.out 
+#mpirun -np 2 -perhost 2 -hostfile $hostlist ./xhpl_intel64_static -n $linpack_N -p $linpack_P -q $linpack_Q -nb $linpack_NB > hpl_output.out 
 #GFLOPS
-grep WC00C2R2 *.out | awk '{ print $7 }'
+#grep WC00C2R2 *.out | awk '{ print $7 }'
 
 #STREAM
 mkdir ~/stream
 cd ~/stream
 wget https://objectstorage.us-phoenix-1.oraclecloud.com/p/Aa4DtCkuLXVw7oc1d2oirEcHJ13UWjSeMPlilKobnBU/n/hpc/b/HPC_BENCHMARKS/o/stream.96GB
 chmod +x stream.96GB
-KMP_AFFINITY=scatter ./stream.96GB > stream_output.out
+#KMP_AFFINITY=scatter ./stream.96GB > stream_output.out
 #BEST RATE MB/s
 grep Triad *.out | awk '{ print $2 }'
 : '
@@ -49,7 +49,7 @@ mkdir ~/qe
 cd ~/qe
 wget https://objectstorage.us-phoenix-1.oraclecloud.com/p/6ACXXrKHqHO4iZofoDv7WtzRGju2vFxHmDPzw2ywWrw/n/hpc/b/HPC_BENCHMARKS/o/espresso-5.4.0-impi.tgz -O - | tar zx
 wget https://objectstorage.us-phoenix-1.oraclecloud.com/p/qwRyCrmsrNv8pr8BiGgfhuxwgxpTmu8Qc0SBqFhNaIs/n/hpc/b/HPC_BENCHMARKS/o/espresso_input.tgz -O - | tar zx
-mpirun -np $numProcs -ppn $processesPerNode -genv LD_LIBRARY_PATH qe:\$LD_LIBRARY_PATH -hostfile \$HOME/bin/hostlist qe/pw.x -input input/pw_1.in
+#mpirun -np $numProcs -ppn $processesPerNode -genv LD_LIBRARY_PATH qe:\$LD_LIBRARY_PATH -hostfile \$HOME/bin/hostlist qe/pw.x -input input/pw_1.in
 
 #NAMD
 mkdir ~/namd
